ca4acccfaabf9d66a07b51bafe8345ec29d2d86b
hamza.mukhtar <hamzamukhtar9819@gmail.com>
2022-09-25 13:45:10 +0500

    Checks account info

--- solana-read-data/src/App.js
+++ solana-read-data/src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { 
+  Connection, 
+  PublicKey, 
+  clusterApiUrl,
+  LAMPORTS_PER_SOL,
+} from '@solana/web3.js';
+
 
 function App() {
+  const [pubAddress, setPubAddress] = useState('');
+  const [balance, setBalance] = useState({amount: 0, address:'', fetched: false})
+  const [accountInfo, setAccountInfo] = useState({info: '', fetched: false})
+
+  const findAccountInfo = (publicAddress, connection) => {
+    connection.getAccountInfo(publicAddress).then(({executable}) => {
+      setAccountInfo({
+        info: executable,
+        fetched: true,
+      })
+    }) 
+  }
+  const findBalance = (publicAddress, connection) => {
+    const balance = connection.getBalance(publicAddress);
+    balance.then((blnc)=>{
+      setBalance({
+        amount: blnc/LAMPORTS_PER_SOL,
+        address: pubAddress,
+        fetched: true,
+      })
+    });
+  }
+  const handleFormSubmit =(e)=> {
+    e.preventDefault();
+    const publicAddress = new PublicKey(pubAddress);
+    const connection = new Connection(clusterApiUrl('devnet'));
+    findBalance(publicAddress, connection);
+    findAccountInfo(publicAddress, connection)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Start your solana Journey</h3>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" value={pubAddress} onChange={(e)=>setPubAddress(e.target.value)}/>
+        <button type="submit">Check SOL Balance</button>
+      </form>
+      {!!balance.fetched && 
+      <>
+      <h4>Balance:{balance.amount}</h4>
+      <h4>Account:{balance.address}</h4>
+      </> }
+      {!!accountInfo.fetched && 
+      <>
+      <h4>executable:{accountInfo.info ? 'Yes' : 'Nope'}</h4>
+      </> }
+
+
     </div>
   );
 }
